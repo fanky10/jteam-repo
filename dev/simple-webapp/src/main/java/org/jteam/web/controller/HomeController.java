0dab8ba6bf6c8cb437b93c1a3ccb2c4b0fb84dad
Facundo Scoccia <fanky10@gmail.com>
2013-01-22 00:02:48 -0300

    ahora tiene forma de proyecto mvc con spring sencillo

---
+++ dev/simple-webapp/src/main/java/org/jteam/web/controller/HomeController.java
@@ -0,0 +1,29 @@
+package org.jteam.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/home")
+    public String home(Model map) {
+		map.addAttribute("output", "HomeController: Passing through...");
+		return "web/home";
+    }
+    @RequestMapping(value = "/compare", method = RequestMethod.GET)
+    public ModelAndView compare(@RequestParam("input1") String input1,
+            @RequestParam("input2") String input2, ModelMap model) {
+ 
+        int result = (input1.compareTo(input2));
+        String inEnglish = (result < 0) ? "less than" : (result > 0 ? "greater than" : "equal to");
+        String output = "According to our Comparator, '" + input1 + "' is " + inEnglish + "'" + input2 + "'";
+        model.addAttribute("output", output);
+        System.out.println("output: "+output);
+        return new ModelAndView("web/compareResult", model);
+    }
+}
---
+++ dev/simple-webapp/src/main/webapp/WEB-INF/pages/web/home.jsp
@@ -0,0 +1,10 @@
+<%@ include file="/WEB-INF/pages/web/common/taglibs.jsp"%>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+    	<c:out value="Mensaje: ${mensaje}"></c:out>
+        <h1>Hello world! </h1>
+    </body>
+</html>

17d98a37ff59c4bf242564f7569e9d021e02ad01
Facundo Scoccia <fanky10@gmail.com>
2013-01-22 12:06:25 -0300

    very simple dependences injection as a resource

--- dev/simple-webapp/src/main/java/org/jteam/web/controller/HomeController.java
+++ dev/simple-webapp/src/main/java/org/jteam/web/controller/HomeController.java
@@ -1,5 +1,7 @@
 package org.jteam.web.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -10,20 +12,35 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value = {"/","/home"})
-    public String home(Model map) {
-		map.addAttribute("mensaje", "Bienvenido al Home!");
+
+	@Resource
+	private String homeMessage;
+
+	@RequestMapping(value = { "/", "/home" })
+	public String home(Model map) {
+		map.addAttribute("mensaje", getHomeMessage());
 		return "web/home";
-    }
-    @RequestMapping(value = "/compare", method = RequestMethod.GET)
-    public ModelAndView compare(@RequestParam("input1") String input1,
-            @RequestParam("input2") String input2, ModelMap model) {
- 
-        int result = (input1.compareTo(input2));
-        String inEnglish = (result < 0) ? "less than" : (result > 0 ? "greater than" : "equal to");
-        String output = "According to our Comparator, '" + input1 + "' is " + inEnglish + "'" + input2 + "'";
-        model.addAttribute("output", output);
-        System.out.println("output: "+output);
-        return new ModelAndView("web/compareResult", model);
-    }
+	}
+
+	@RequestMapping(value = "/compare", method = RequestMethod.GET)
+	public ModelAndView compare(@RequestParam("input1") String input1,
+			@RequestParam("input2") String input2, ModelMap model) {
+
+		int result = (input1.compareTo(input2));
+		String inEnglish = (result < 0) ? "less than"
+				: (result > 0 ? "greater than" : "equal to");
+		String output = "According to our Comparator, '" + input1 + "' is "
+				+ inEnglish + "'" + input2 + "'";
+		model.addAttribute("output", output);
+		System.out.println("output: " + output);
+		return new ModelAndView("web/compareResult", model);
+	}
+
+	public String getHomeMessage() {
+		return homeMessage;
+	}
+
+	public void setHomeMessage(String homeMessage) {
+		this.homeMessage = homeMessage;
+	}
 }
